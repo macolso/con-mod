d9505fac5a7603a8d261bdd191be1a296061684c
Adam Reese <adam@reese.io>
2025-02-05 10:15:53 -0500

    Implementing fetch and parse html

--- component/src/lib.rs
+++ component/src/lib.rs
@@ -1,4 +1,5 @@
 use bindings::exports::component::content_moderator::moderator::Guest;
+use html2text::from_read;
 use spin_sdk::http::{Request, Response};
 
 #[allow(warnings)]
@@ -8,7 +9,22 @@ struct Component;
 
 impl Guest for Component {
     fn moderate(url: String) -> Result<String, String> {
-        todo!("implement me")
+        // hardcode url for testing
+        let url = "https://www.engadget.com/2017-05-15-not-hotdog-app-hbo-silicon-valley.html";
+
+        // fetch the url
+        let result: Result<Response, _> =
+            spin_executor::run(spin_sdk::http::send(Request::get(url)));
+
+        let resp = result.unwrap();
+
+        // convert the html response to text format
+        let document = from_read(&resp.body()[..], 80).unwrap();
+
+        // dump for testing
+        println!("{document}");
+
+        Ok(String::from("done"))
     }
 }
 
